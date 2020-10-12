a0b08ffed75d17b36d4a0566d7505777558f7c90
Jhenry019 <Jhenry502@gmail.com>
2020-10-11 20:40:09 -0400

    add user authentication, css changes and student detail

--- frontend/urls.py
+++ frontend/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('logout', views.logout_user, name='logout'),
+    path('transactions', views.blocks, name='transactions'),
     path('students', views.students, name='students'),
-    path('student-detail/<id>', views.student_detail, name='student-detail')
+    path('student/<int:id>', views.student_detail, name='student')
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -1,10 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
 from blockchain_records.models import Block
 from student_records.models import StudentRecord
 
 
 def index(request):
+
+  if request.method == 'POST':
+    username = request.POST['username']
+    password = request.POST['password']
+
+    user = authenticate(
+            request, 
+            username=username, 
+            password=password
+           )
+    if user is not None:
+      login(request, user)
+      return redirect('transactions')
+
+  else:
+    context = {
+      'user': request.user
+    }
+
+    return render(request, 'partials/_login.html', context)
+
+def logout_user(request):
+  logout(request)
+  return redirect('index')
+
+def blocks(request):
   context = {
     'blocks': Block.objects.order_by('-timestamp')
   }
